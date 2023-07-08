7fcbdb1413377aa27f79a3bbbf4a4e0c55ed83d5
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-07-08 22:30:12 +0530

    chore:workaround in android for XMTP

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,6 @@
+import { Web3Modal } from "@web3modal/react-native";
 import { Stack } from "expo-router";
 import React from "react";
-import { Web3Modal } from "@web3modal/react-native";
-
-
 const projectId = "6097f40a8f4f91e37e66cf3a5ca1fba2";
 
 const providerMetadata = {
@@ -16,8 +14,6 @@ const providerMetadata = {
   },
 };
 
-
-
 const StackLayout = () => {
   return (
     <>
@@ -28,13 +24,13 @@ const StackLayout = () => {
             headerShown: false,
             animation: "fade_from_bottom",
           }}
-        />
+          />
         <Stack.Screen
           name="settings/index"
           options={{
             headerTitle: "Settings",
           }}
-        />
+          />
       </Stack>
       <Web3Modal projectId={projectId} providerMetadata={providerMetadata} />
     </>
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,47 @@
-
+import * as XMTP from "@xmtp/react-native-sdk";
 import { useWeb3Modal } from "@web3modal/react-native";
-import { Link } from "expo-router";
-
-import React from "react";
+import React, { useCallback } from "react";
 import { Button, View } from "react-native";
 
 export default function index() {
+  const { open, isConnected, address, provider } = useWeb3Modal();
 
-  const { open, isConnected, address, isOpen, close } = useWeb3Modal();
+  //Signer Interface
+  const signer = {
+    getAddress: function (): Promise<string> {
+      return address;
+    },
+    signMessage: function (
+      message: string | ArrayLike<number>
+    ): Promise<string> {
+      return provider?.request({
+        method: "personal_sign",
+        params: [address, message],
+      });
+    },
+  };
+
+  const handleConnect = useCallback(async () => {
+    try {
+      console.log("Connecting to XMTP....");
+
+      const client = await XMTP.Client.create(signer, "dev");
+
+      console.log("Clinet", client);
+    } catch (error) {
+      console.log("Error in connecting To XMTP", error);
+    }
+  }, []);
 
   return (
     <View>
-      <Link href={"/Home"}>
-        <Button onPress={open} title="Connect Wallet" />
-      </Link>
+      <View style={{ width: "80%", alignSelf: "center" }}>
+        <Button
+          onPress={open}
+          title={isConnected ? "Connected" : "Connect Wallet"}
+        />
+        {isConnected && <Button title="Enable Chat" onPress={handleConnect} />}
+      </View>
     </View>
   );
 }
